d7262a3de97992f1515f4f5b6aabba943ce5d295
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-04-11 22:30:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def img_button(img_path):
         st.image(img_path, use_container_width=True)
         if st.button("Go", key=img_path, use_container_width=True):
             return True
-
+st.title("A Geography Channel Productions")
 col1, col2, col3 = st.columns(3)
 
 with col1:
