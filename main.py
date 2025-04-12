4ad6a8d5e8774bfd08a9c99510c0e4782248e5b7
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-04-11 22:16:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -24,23 +24,20 @@ def img_button(img_path):
     if submitted:
         return True
 
-col1, col2, col3, col4 = st.columns(4)
+col1, col2, col3 = st.columns(3)
 
 with col1:
     if img_button("FAN.png"):
         st.text("FAN clicked")
 
 with col2:
-    if img_button("FAN.png"):
+    if img_button("WARS.png"):
         st.text("FAN clicked")
 
 with col3:
-    if img_button("FAN.png"):
+    if img_button("CENTURIES.png"):
         st.text("FAN clicked")
         
-with col4:
-    if img_button("FAN.png"):
-        st.text("FAN clicked")
 
 
 
