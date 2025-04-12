58479b0d6c717997d6969ca1ffa51249c51dcc13
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-04-11 21:49:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 import streamlit as st
 st.title("Title")
+if st.button("Game 1"):
+  select1 = 1
+if st.button("Game 2"):
+  select1 = 2
+if st.button("Game 3"):
+  select1 = 3
+if st.button("Game 4"):
+  select1 = 4
+st.session_state["selection1"] = select1
+if st.session_state["selection1"] == 1:
+  st.title(f"Game {st.session_state["selection1"]}")
+  st.text("mo stuff for game 1")
+if st.session_state["selection1"] == 2:
+  st.title(f"Game {st.session_state["selection1"]}")
+  st.text("mo stuff for game 2")
+if st.session_state["selection1"] == 3:
+  st.title(f"Game {st.session_state["selection1"]}")
+  st.text("mo stuff for game 3")
+if st.session_state["selection1"] == 4:
+  st.title(f"Game {st.session_state["selection1"]}")
+  st.text("mo stuff for game 4")
