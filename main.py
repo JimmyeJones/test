01f400d9f27eeb8102fc20e22f82bb9821a11156
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-04-11 22:21:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,17 +11,8 @@ def img_button(img_path):
     img_base64 = image_to_base64(img_path)
     
     # Use a form to simulate an image button
-    with st.form("image_button_form"):
-        # Fake image "button" using HTML and CSS
-        st.markdown(f"""
-            <button type="submit" style="border:none; background:none; padding:0;">
-                <img src="data:image/png;base64,{img_base64}" width="150" style="cursor:pointer;" />
-            </button>
-        """, unsafe_allow_html=True)
-        submitted = st.form_submit_button("hidden_button")  # label won't show
-    
-    # Do something when image is clicked
-    if submitted:
+    st.image(img_base64)
+    if st.button("Go"):
         return True
 
 col1, col2, col3 = st.columns(3)
