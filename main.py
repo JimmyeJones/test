6a09f495861b9c32c8973f44951e8abe15efe0de
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-04-11 22:26:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 import streamlit as st
 import base64
 def img_button(img_path):
-    
-    # Load local image and convert to base64
-    def image_to_base64(img_path):
-        with open(img_path, "rb") as f:
-            data = f.read()
-        return base64.b64encode(data).decode()
-    
-    img_base64 = image_to_base64(img_path)
-    
-    # Use a form to simulate an image button
-    st.image(img_path)
-    if st.button("Go"):
+    st.image(img_path, use_container_width=True)
+    if st.button("Go", key=img_path):
         return True
 
 col1, col2, col3 = st.columns(3)
